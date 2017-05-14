d2106d6906d4a3237766832338a9fd116be669ac
unknown <pebcc12@gmail.com>
2017-05-14 02:21:17 -0500

    boilerplate files upload

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+// var stormpath = require('express-stormpath');
+ 
+var app = express();
+ 
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get("/", function(req, res) {
+	res.render('home', {
+	    title: 'Welcome'
+  	});
+});
+/*app.use(stormpath.init(app, {
+  expand: {
+    customData: true
+  }
+}));
+ 
+app.get('/', stormpath.getUser, function(req, res) {
+  res.render('home', {
+    title: 'Welcome'
+  });
+});
+ 
+app.on('stormpath.ready',function(){
+  console.log('Stormpath Ready');
+});
+*/
+
+app.listen(3000);
