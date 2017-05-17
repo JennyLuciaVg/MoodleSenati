4bc4b7ffea5abad4c4680a74f584b4d58b92954b
Jenny Vega <jenny@landgorilla.com>
2017-05-16 20:44:36 -0500

    view actas

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,12 @@ module.exports = function(app) {
     app.get('/grade/pondera01', function(req, res) {
         res.render('pages/ponderaciones');
     });
+
+    app.get('/grade/acta_notas', function(req, res) {
+        res.render('pages/acta_notas_sinfo');
+    });
+
+    app.get('/grade/acta_notas02', function(req, res) {
+        res.render('pages/acta_notas_sinfo_fusionadas');
+    });
 };
